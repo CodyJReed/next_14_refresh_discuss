850e5e207d46e9b6d3ad4e88dc75e02f6a516799
CodyJReed <cody_j_reed@proton.me>
2026-02-05 12:57:25 -0500

    refactor header to prevent all pages from rendering dynamically; disable eslinst any rule on auth.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,3 @@
-import { Button } from "@nextui-org/react";
-import * as actions from '@/actions'
-import {auth} from '@/auth'
-
-import Profile from "@/components/profile";
-
-export default async function Home() {
-  const session = await auth()
-
-  return (
-    <div className="flex flex-col gap-4 m-4">
-      <form action={actions.signIn}>
-        <Button type="submit">
-          Sign In
-        </Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">
-          Sign Out
-        </Button>
-      </form>
-
-      {
-        session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed out.</div>
-      }
-      <Profile/>
-    </div>
-  );
+export default function Home() {
+  return <div>Home Page</div>;
 }
--- src/auth.ts
+++ src/auth.ts
@@ -3,29 +3,35 @@ import Github from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./db";
 
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
-    throw new Error('Missing github oauth credentials')
+  throw new Error("Missing github oauth credentials");
 }
 
-export const {handlers: {GET, POST}, auth, signOut, signIn} = NextAuth({
-    adapter: PrismaAdapter(db),
-    providers: [
-        Github({
-            clientId: GITHUB_CLIENT_ID,
-            clientSecret: GITHUB_CLIENT_SECRET
-        })
-    ],
-    callbacks: {
-        // Typically not required, bug fix in current version
-        async session({session, user}: any) {
-            if (session && user) {
-                session.user.id = user.id
-            }
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    // Typically not required, bug fix in current version
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async session({ session, user }: any) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
 
-            return session;
-        }
-    }
-})
+      return session;
+    },
+  },
+});
