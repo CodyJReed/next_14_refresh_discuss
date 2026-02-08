3bee600701c292daa9081e66a9d7ce8a6c2f02df
CodyJReed <cody_j_reed@proton.me>
2026-02-08 15:11:39 -0500

    refactor ../page.tsx to include dynamic content and styling

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <div>Topic</div>;
+interface PageProps {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const { slug } = await params;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+
+      <div></div>
+    </div>
+  );
 }
