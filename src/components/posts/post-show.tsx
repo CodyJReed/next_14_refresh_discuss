b902cf0536e0132ab5f2dd86893f9a557bcf53a9
CodyJReed <cody2j2reed@gmail.com>
2026-02-19 09:16:42 -0500

    add artifical response time to post-show; introduce suspense; add fallback skeleton

--- src/components/posts/post-show.tsx
+++ src/components/posts/post-show.tsx
@@ -6,6 +6,7 @@ interface PostShowProps {
 }
 
 export default async function PostShow({ id }: PostShowProps) {
+  await new Promise((resolve) => setTimeout(resolve, 2500));
   const post = await db.post.findFirst({
     where: { id },
   });
